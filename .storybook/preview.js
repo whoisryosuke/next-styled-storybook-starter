0b3e4740f399cf1daa2dbc80be40c8761a4abed7
Ryosuke <ryosuke.san.hana@gmail.com>
2020-10-28 22:23:27 -0700

    :shirt: lint

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React from 'react'
 import { ThemeProvider } from 'styled-components'
-import theme from "../themes/light"
+import theme from '../themes/light'
 
-export const decorators = [(Story) => <ThemeProvider theme={theme}><Story/></ThemeProvider>];
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <Story />
+    </ThemeProvider>
+  ),
+]
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
